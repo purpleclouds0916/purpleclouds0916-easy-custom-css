0d3cea0a0b352542ca74ff64274162cce35a80b8
Shiun Murata <purpleclouds0916@gmail.com>
2023-09-02 13:23:57 +0900

    vanilla-extractを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import linaria from '@linaria/vite';
+
+import { defineConfig } from 'vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), linaria()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    vanillaExtractPlugin({
+      identifiers: ({ hash }) => `prefix_${hash}`,
+    }),
+  ],
 });
